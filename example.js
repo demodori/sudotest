8100ab0a3823bd3bb4d093918f9cac260292437a
hoataaa <hoataaa@gmail.com>
2023-09-27 17:36:18 +0900

    no message

--- example.js
+++ example.js
@@ -37,8 +37,8 @@ controls.enablePan = false;
 controls.enableDamping = true;
 
 const dracoLoader = new DRACOLoader();
-// dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
-dracoLoader.setDecoderPath( './' );
+dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
+// dracoLoader.setDecoderPath( './' );
 
 const loader = new GLTFLoader();
 loader.setDRACOLoader( dracoLoader );
